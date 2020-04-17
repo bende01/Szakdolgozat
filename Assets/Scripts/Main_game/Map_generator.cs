5f20f3dcea7b76ce0a66ada1693f1c1220b222db
Lontai Bendegúz <lontai.bende@hotmail.com>
2020-04-17 15:52:05 +0200

    Smoothed map (more straight walk path)

--- Assets/Scripts/Main_game/Map_generator.cs
+++ Assets/Scripts/Main_game/Map_generator.cs
@@ -17,9 +17,11 @@ public class Map_generator : MonoBehaviour
         var map = GenerateArray(width, height, true);
 
         //map = PerlinNoise(map, 0f);
-        map = RandomWalkTop(map, 0f);
+        map = RandomWalkTopSmoothed(map, 0f,3);
 
         RenderMap(map, tilemap, tile);
+
+        UpdateMap(map, tilemap);
 
         
 
@@ -32,7 +34,7 @@ public class Map_generator : MonoBehaviour
     }
 
 
-    public static int[,] GenerateArray(int width, int height, bool empty)
+    public static int[,] GenerateArray(int width, int height, bool empty)  //Generates the map
     {
         int[,] map = new int[width, height];
         for (int x = 0; x < map.GetUpperBound(0); x++)
@@ -56,6 +58,7 @@ public class Map_generator : MonoBehaviour
     {
         //Clear the map (ensures we dont overlap)
         tilemap.ClearAllTiles();
+
         //Loop through the width of the map
         for (int x = 0; x < map.GetUpperBound(0); x++)
         {
@@ -72,15 +75,15 @@ public class Map_generator : MonoBehaviour
     }
 
 
-    public static void UpdateMap(int[,] map, Tilemap tilemap) //Takes in our map and tilemap, setting null tiles where needed
+    public static void UpdateMap(int[,] map, Tilemap tilemap) //In game update if needed
     {
         for (int x = 0; x < map.GetUpperBound(0); x++)
         {
             for (int y = 0; y < map.GetUpperBound(1); y++)
             {
-                //We are only going to update the map, rather than rendering again
-                //This is because it uses less resources to update tiles to null
-                //As opposed to re-drawing every single tile (and collision data)
+                
+                // Things we want to do with map in game
+
                 if (map[x, y] == 0)
                 {
                     tilemap.SetTile(new Vector3Int(x, y, 0), null);
@@ -89,58 +92,50 @@ public class Map_generator : MonoBehaviour
         }
     }
 
-    public static int[,] PerlinNoise(int[,] map, float seed)
-    {
-        int newPoint;
-        //Used to reduced the position of the Perlin point
-        float reduction = 0.5f;
-        //Create the Perlin
-        for (int x = 0; x < map.GetUpperBound(0); x++)
-        {
-            newPoint = Mathf.FloorToInt((Mathf.PerlinNoise(x, seed) - reduction) * map.GetUpperBound(1));
 
-            //Make sure the noise starts near the halfway point of the height
-            newPoint += (map.GetUpperBound(1) / 2);
-            for (int y = newPoint; y >= 0; y--)
-            {
-                map[x, y] = 1;
-            }
-        }
-        return map;
-    }
 
-    public static int[,] RandomWalkTop(int[,] map, float seed)
+    public static int[,] RandomWalkTopSmoothed(int[,] map, float seed, int minSectionWidth)
     {
-        //Seed our random
+        //Seed random
         System.Random rand = new System.Random(seed.GetHashCode());
 
-        //Set our starting height
+        //Determine the starting position
         int lastHeight = Random.Range(0, map.GetUpperBound(1));
 
-        //Cycle through our width
-        for (int x = 0; x < map.GetUpperBound(0); x++)
-        {
-            //Flip a coin
-            int nextMove = rand.Next(2);
+        //Used to determine which direction to go
+        int nextMove;
 
-            //If heads, and we aren't near the bottom, minus some height
-            if (nextMove == 0 && lastHeight > 2)
+        //Used to keep track of the current sections width
+        int sectionWidth = 0;
+
+        //Work through the array width
+        for (int x = 0; x <= map.GetUpperBound(0); x++)
+        {
+            //Determine the next move -- 0 down -- 1 up
+            nextMove = rand.Next(2);
+
+            //Only change the height if we have used the current height more than the minimum required section width
+            if (nextMove == 0 && lastHeight > 0 && sectionWidth > minSectionWidth)
             {
                 lastHeight--;
+                sectionWidth = 0;
             }
-            //If tails, and we aren't near the top, add some height
-            else if (nextMove == 1 && lastHeight < map.GetUpperBound(1) - 2)
+            else if (nextMove == 1 && lastHeight < map.GetUpperBound(1) && sectionWidth > minSectionWidth)
             {
                 lastHeight++;
+                sectionWidth = 0;
             }
+            //Increment the section width
+            sectionWidth++;
 
-            //Circle through from the lastheight to the bottom
+            //Work our way from the height down to 0
             for (int y = lastHeight; y >= 0; y--)
             {
                 map[x, y] = 1;
             }
         }
-        //Return the map
+
+        //Return the modified map
         return map;
     }
 }
