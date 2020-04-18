2a3b7fbb5a7bc7a8b3201db6ffe02b281a4faa01
Lontai Bendegúz <lontai.bende@hotmail.com>
2020-04-18 17:24:27 +0200

    Added rule tile, reworked map generator algorithm,

--- Assets/Scripts/Main_game/Map_generator.cs
+++ Assets/Scripts/Main_game/Map_generator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -8,21 +9,27 @@ public class Map_generator : MonoBehaviour
     Tilemap tilemap;
 
     public TileBase tile;
-    public int width, height = 100;
+    public int width = 100;
+    private int height;
+
+    public int minSectionWidth;
+    public int maxSectionWidth;
 
 
     void Start()
     {
+        height = width / minSectionWidth * 6 + width / minSectionWidth * 8 + 2;  //== max possible elevation + max possible demotion
         tilemap = GetComponent<Tilemap>();
         var map = GenerateArray(width, height, true);
 
         //map = PerlinNoise(map, 0f);
-        map = RandomWalkTopSmoothed(map, 0f,3);
+        map = RandomWalkTopSmoothed(map,minSectionWidth,maxSectionWidth);
+        //map = RandomWalkTop(map, 123123);
 
         RenderMap(map, tilemap, tile);
 
-        UpdateMap(map, tilemap);
-
+        //UpdateMap(map, tilemap);
+        tilemap.CompressBounds();
         
 
     }
@@ -92,50 +99,125 @@ public class Map_generator : MonoBehaviour
         }
     }
 
-
-
-    public static int[,] RandomWalkTopSmoothed(int[,] map, float seed, int minSectionWidth)
+    public static int[,] RandomWalkTop(int[,] map, float seed)
     {
-        //Seed random
+        //Seed our random
         System.Random rand = new System.Random(seed.GetHashCode());
 
-        //Determine the starting position
-        int lastHeight = Random.Range(0, map.GetUpperBound(1));
+        //Set our starting height
+        int lastHeight = UnityEngine.Random.Range(0, map.GetUpperBound(1));
 
-        //Used to determine which direction to go
-        int nextMove;
-
-        //Used to keep track of the current sections width
-        int sectionWidth = 0;
-
-        //Work through the array width
-        for (int x = 0; x <= map.GetUpperBound(0); x++)
+        //Cycle through our width
+        for (int x = 0; x < map.GetUpperBound(0); x++)
         {
-            //Determine the next move -- 0 down -- 1 up
-            nextMove = rand.Next(2);
+            //Flip a coin
+            int nextMove = rand.Next(2);
 
-            //Only change the height if we have used the current height more than the minimum required section width
-            if (nextMove == 0 && lastHeight > 0 && sectionWidth > minSectionWidth)
+            //If heads, and we aren't near the bottom, minus some height
+            if (nextMove == 0 && lastHeight > 2)
             {
                 lastHeight--;
-                sectionWidth = 0;
             }
-            else if (nextMove == 1 && lastHeight < map.GetUpperBound(1) && sectionWidth > minSectionWidth)
+            //If tails, and we aren't near the top, add some height
+            else if (nextMove == 1 && lastHeight < map.GetUpperBound(1) - 2)
             {
                 lastHeight++;
-                sectionWidth = 0;
             }
-            //Increment the section width
-            sectionWidth++;
 
-            //Work our way from the height down to 0
+            //Circle through from the lastheight to the bottom
             for (int y = lastHeight; y >= 0; y--)
             {
                 map[x, y] = 1;
             }
         }
+        //Return the map
+        return map;
+    }
+    public static int[,] RandomWalkTopSmoothed(int[,] map, int minSectionWidth, int maxSectionWidth)
+    {
+        //Seed random
+        
+
+
+        int x,y;
+        
+        
+        y = map.GetUpperBound(0) / minSectionWidth * 8+2; //Start from here to not get out of boudns
+
+        int thickness=3;
+
+        map = GenerateSection(map,0,y,7,thickness);
+
+
+       
+        int nextWidth;
+        int nextHeight;
+
+        
+        int currentSectionWidth;
+
+        //Work through the array width
+        for ( x = 8; x <= map.GetUpperBound(0)-maxSectionWidth; x++)
+        {
+            //Determine the next move -- 0 down -- 1 up
+            nextHeight = UnityEngine.Random.Range(-8,6);
+
+            //Only change the height if we have used the current height more than the minimum required section width
+            if (nextHeight+y <= y+3 && nextHeight+y>=y-thickness+1)
+            {
+                nextWidth = UnityEngine.Random.Range(1, 5);
+                
+                currentSectionWidth = UnityEngine.Random.Range(minSectionWidth, maxSectionWidth);
+                y = y + nextHeight;
+                x = x + nextWidth;
+                map = GenerateSection(map, x, y, currentSectionWidth, thickness);
+
+                x = x + currentSectionWidth - 1;
+                
+
+            }
+            else if (nextHeight+y >y+3)
+            {
+                nextWidth = UnityEngine.Random.Range(-4, 4);
+                
+                currentSectionWidth = UnityEngine.Random.Range(minSectionWidth, maxSectionWidth);
+                y = y + nextHeight;
+                x = x + nextWidth;
+                map = GenerateSection(map, x, y, currentSectionWidth, thickness);
+
+                x = x + currentSectionWidth - 1;
+            }
+            else
+            {
+                nextWidth = UnityEngine.Random.Range(-4,6);
+               
+                currentSectionWidth = UnityEngine.Random.Range(minSectionWidth, maxSectionWidth);
+                y = y + nextHeight;
+                x = x + nextWidth;
+                map = GenerateSection(map, x, y, currentSectionWidth, thickness);
+
+                x = x + currentSectionWidth - 1;
+            }
+            //Increment the section width
+           
+        }
 
         //Return the modified map
+        return map;
+    }
+
+
+
+    public static int[,] GenerateSection(int[,] map,int x, int y, int sectionWidth, int thickness)
+    {
+        for (int i = x; i < x+sectionWidth; i++)
+        {
+            for (int j = 0; j < thickness; j++)
+            {
+                map[i, y - j] = 1;
+            }
+        }
+
         return map;
     }
 }
